622f6b65c0687f7dd09f9025a951fb12b15bca1e
marc.segbetse <edemmarc@hotmail.fr>
2017-03-07 11:53:14 +0100

    first commit

---
+++ ressources/leaflet_poc/map.js
@@ -0,0 +1,50 @@
+window.onload=function() {
+  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'your.mapbox.project.id',
+      accessToken: 'your.mapbox.public.access.token'
+  }).addTo(mymap);
+
+  var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+  var circle = L.circle([51.508, -0.11], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 500
+  }).addTo(mymap);
+
+  var polygon = L.polygon([
+      [51.509, -0.08],
+      [51.503, -0.06],
+      [51.51, -0.047]
+  ]).addTo(mymap);
+
+  marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+  circle.bindPopup("I am a circle.");
+  polygon.bindPopup("I am a polygon.");
+
+  var popup = L.popup()
+      .setLatLng([51.5, -0.09])
+      .setContent("Je suis une popup.")
+      .openOn(mymap);
+
+      function onMapClick(e) {
+      alert("You clicked the map at " + e.latlng);
+  }
+
+  mymap.on('click', onMapClick);
+
+  var popup = L.popup();
+
+  function onMapClick(e) {
+      popup
+          .setLatLng(e.latlng)
+          .setContent("You clicked the map at " + e.latlng.toString())
+          .openOn(mymap);
+  }
+
+  mymap.on('click', onMapClick);
+};
