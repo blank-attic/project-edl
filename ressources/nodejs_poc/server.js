79408a41e9180d72420719a0e3ba2498e6be4e4f
marc.segbetse <edemmarc@hotmail.fr>
2017-03-20 18:38:55 +0100

    REST API

---
+++ ressources/nodejs_poc/server.js
@@ -0,0 +1,26 @@
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
