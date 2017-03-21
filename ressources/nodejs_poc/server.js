8729f1004f8aec976d308d64d1fc1f885e8b8379
marc.segbetse <edemmarc@hotmail.fr>
2017-03-21 16:49:41 +0100

    creation d'une base de donnees mongodb

--- ressources/nodejs_poc/server.js
+++ ressources/nodejs_poc/server.js
@@ -10,17 +10,58 @@ var port = process.env.PORT || 8080;
 var router = express.Router();
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+router.get('/', function(req, res, next) {
+  res.json({ message: 'hooray! welcome to our api!' });
+  next();
 });
 
-// more routes for our API will happen here
+router.get('/toto', function(req, res, next) {
+  res.json({ message: 'toto' });
+  next();
+});
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+router.get('/alert', function(req, res, next) {
+    res.json({message: 'alert'});
+    next();
+});
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+
+router.get('/paa', function(req, res){
+  mon_paa.find(null, function (err, comms) {
+    if (err) { throw err; }
+    console.log(comms);
+  });
+});
+
+  // more routes for our API will happen here
+
+  // REGISTER OUR ROUTES -------------------------------
+  // all of our routes will be prefixed with /api
+  app.use('/api', router);
+
+  // START THE SERVER
+  // =============================================================================
+  app.listen(port);
+  console.log('Magic happens on port ' + port);
+  //
+
+  var mongoose   = require('mongoose');
+  mongoose.connect('mongodb://localhost/paad', function(err) {
+    if (err) { throw err; }
+  });
+
+  var schema       = mongoose.Schema;
+
+  var paa_schema   = new schema({
+    name: String
+  });
+
+  var nom_paa = mongoose.model('nouvelle_paa', paa_schema);
+
+  var nom = new nom_paa({name:'belleville_citoyenne'});
+
+  nom.save(function (err) {
+    if (err) { throw err; }
+    console.log('quelque chose a été ajoutée à la base de données');
+    mongoose.connection.close();
+  });
