a45f23e4343e5698f3cf8bcd34b8dd18b1a97933
MarcEdem <edemmarc@hotmail.fr>
2017-03-28 18:50:11 +0200

    enhanced geocoder

--- ressources/nodejs_poc/geocoder.js
+++ ressources/nodejs_poc/geocoder.js
@@ -2,11 +2,12 @@ var OpenGeocoder = require('node-open-geocoder');
 
 var geo = new OpenGeocoder();
 
-geo.geocode('38 rue des amandiers, paris, france, 75020', function(err, res) {
+geo.geocode('38 rue des amandiers 75020, Paris, France', function(err, res) {
 	if (err) {
 		return err;
 	} else {
 		// return res;
-    console.log(res);
+    console.log(res[0].display_name);
+		// console.log()
 	}
 });
