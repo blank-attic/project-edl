426324e34f7985d51cccfbc485ba4e9cb33cbb58
MarcEdem <edemmarc@hotmail.fr>
2017-04-14 19:10:29 +0200

    api ready for use

--- ressources/nodejs_poc/server.js
+++ ressources/nodejs_poc/server.js
@@ -1,35 +1,29 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var bienvenue = require('./routes/bienvenue.js');
+var structures = require('./routes/structures.js');
+var structures_ordre_alphabetique = require('./routes/structures_ordre_alphabetique.js');
+var structures_bap = require('./routes/structures_bap.js');
+var structures_portes_du_20 = require('./routes/structures_portes_du_20eme.js');
+var structures_arrodissement = require('./routes/structures_arrondissement.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.json({ message: "Bienvenue sur l'API des permanences d'accès aux droits du 20ème!" });
-  next();
-});
-
-router.get('/paad', function(req, res){
-  paad_model.find(function (err, paa) {
-    if(err) res.send(err);
-    res.json(paa);
-  });
-});
-
-app.use('/api', router);
-
-app.listen(port);
-console.log('En écoute sur le port ' + port);
-
-var mongoose = require('mongoose');
-
-mongoose.Promise = require('bluebird');
+app.use('/api', bienvenue);
 
 mongoose.connect('mongodb://localhost/permanence_acces_aux_droits');
 
-var paad_model = require('./models/paad_model.js');
+app.use('/api/paad', structures);
+app.use('/api/paad_ordre_alphabetique', structures_ordre_alphabetique);
+app.use('/api/paad_bap', structures_bap);
+app.use('/api/paad_portes_du_20eme', structures_portes_du_20);
+app.use('/api/paad_arrondissement', structures_arrodissement);
+
+app.listen(port);
+console.log('En écoute sur le port ' + port);
