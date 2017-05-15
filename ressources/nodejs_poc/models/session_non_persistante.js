a5feadbb92f6efe07045bf864f6f27e3670109dd
MarcEdem <edemmarc@hotmail.fr>
2017-05-15 18:51:57 +0200

    session storage

---
+++ ressources/nodejs_poc/models/session_non_persistante.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose'),
+timestamps = require('mongoose-timestamp'),
+schema = mongoose.Schema,
+session_non_persistante;
+
+var session_schema = new schema({
+  _id: {
+    type: String,
+    required: true
+  },
+  nom_paad: {
+    type: String,
+    required: true
+  }
+});
+
+session_schema.plugin(timestamps,  {
+  createdAt: 'date_ajout',
+  updatedAt: 'date_modification'
+});
+
+session_non_persistante = mongoose.model('session_non_persistante', session_schema);
+
+module.exports = session_non_persistante;
