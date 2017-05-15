a64e0225602a59e7996cc9522f5a8e66b1e7a20d
MarcEdem <edemmarc@hotmail.fr>
2017-05-15 22:57:16 +0200

    api ready for use

--- ressources/nodejs_poc/server.js
+++ ressources/nodejs_poc/server.js
@@ -12,10 +12,9 @@ var structures_bap = require('./routes/structures_bap.js');
 var structures_portes_du_20 = require('./routes/structures_portes_du_20eme.js');
 var structures_arrodissement = require('./routes/structures_arrondissement.js');
 
-var connexion = require('./routes/connexion.js');
+var connexion_get = require('./routes/connexion_get.js');
+var connexion_post = require('./routes/connexion_post.js');
 var deconnexion = require('./routes/deconnexion.js');
-var session_persistante = require('./models/session_persistante.js');
-var session_non_persistante = require('./models/session_non_persistante.js');
 
 var vide = require('is-empty');
 
@@ -38,54 +37,16 @@ app.use(session({
 }));
 
 app.set('view engine', 'ejs');
-
 app.set('views', path.join(__dirname, './vues'));
 app.engine('html', require('ejs').renderFile);
 
+app.use('/statiques',express.static(path.join(__dirname,'/statiques')));
+
 mongoose.connect('mongodb://localhost/permanence_acces_aux_droits');
 
-app.get('/connexion', function(req, res) {
-  session_non_persistante.findOne({'_id':req.sessionID}, function(err, session) {
-      if (err) {
-        res.render('pages/connexion.ejs', {message: 'Erreur de connexion!'});
-        return;
-      } else if (vide(session)) {
-        session_persistante.findOne({'_id':req.sessionID}, function(err, session) {
-          if (err) {
-            res.render('pages/connexion.ejs', {message: 'Erreur de connexion!'});
-            return;
-          } else if (vide(session)) {
-            res.render('pages/connexion.ejs', {message: ''});
-          } else {
-            res.render('pages/bienvenue.ejs', {nom_utilisateur: session.nom_paad});
-            return;
-          }
-        });
-        return;
-      } else {
-        res.render('pages/bienvenue.ejs', {nom_utilisateur: session.nom_paad});
-        req.connection.on('close', function() {
-          session_non_persistante.remove({'_id':req.sessionID}, function(err){
-            if (err) {
-              res.redirect('/connexion');
-            } else {
-              req.session.regenerate(res.render('pages/deconnexion.ejs', {message: 'Déconnexion réussie!'}));
-              return;
-            }
-          });
-        })
-        return;
-      }
-    });
-    return;
-});
-
-app.get('/reconnexion', function(req,res) {
-  res.render('pages/connexion.ejs', {message: ''});
-  return;
-});
-
-app.use('/statiques',express.static(path.join(__dirname,'/statiques')));
+app.use('/connexion', connexion_get);
+connexion_post(app);
+deconnexion(app);
 
 app.use('/api', bienvenue);
 
@@ -103,13 +64,14 @@ app.get('/liste.html', function(req, res) {
   res.sendFile(path.join(__dirname,'/liste.html'));
 });
 
+app.get('ajout', function(req, res) {
+  res.render('')
+})
 app.use('/api/paad', structures);
 app.use('/api/paad_ordre_alphabetique', structures_ordre_alphabetique);
 app.use('/api/paad_bap', structures_bap);
 app.use('/api/paad_portes_du_20eme', structures_portes_du_20);
 app.use('/api/paad_arrondissement', structures_arrodissement);
-connexion(app);
-deconnexion(app);
 
 app.listen(port);
 console.log('En écoute sur le port ' + port);
